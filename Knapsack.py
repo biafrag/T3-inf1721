b726428a6dd565c0d78dda9b5871d660befd733d
Pedro Magalhaes <pfsmagalhaes@gmail.com>
2018-12-03 09:23:43 -0200

    Ajustando para usar o pre e gerar a quantidade de itens utlizada

--- Knapsack.py
+++ Knapsack.py
@@ -10,58 +10,43 @@ class KnapSack(object):
         m = [[0 for x in range(self.w+1)] for y in range(self.n+1)] # memoria matriz NxW
         #print(m)
         pre = [[(-1,-1,-1) for x in range(self.w+1)]for y in range(self.n+1) ] # memoria matriz NxW
-        qtAnt = 1
-        for i in range(1,self.n + 1):
+
+        for i in range(1,self.n + 1): # for de 1 até n, o item da posicao 0 é considerado o item 1
             for tamanhoRestante in range(self.w + 1):
-                #Se tamanho da mochila for maior que o tamanho restante
+                #Se tamanho do item for maior que o tamanho restante
                 if(self.tamanhos[i-1] > tamanhoRestante):
                     m[i][tamanhoRestante] =  m[i-1][tamanhoRestante]
-                    pre[i][tamanhoRestante] = (i-1,tamanhoRestante,qtAnt)
+                    pre[i][tamanhoRestante] = (i-1,tamanhoRestante)
                 else:
-                    max = m[i-1][tamanhoRestante]
+                    maximo = m[i-1][tamanhoRestante]
                     premax = (i-1,tamanhoRestante)
-                    incluiItem = False
                     for k in range(1,self.q+1):
                         if(k*self.tamanhos[i - 1] > tamanhoRestante):
                             break
                         else:
                             incluiItem = m[i-1][tamanhoRestante - (k*self.tamanhos[i-1])] + k*self.valores[i-1]
-                            if (m[i-1][tamanhoRestante] > max):
-                                max = m[i-1][tamanhoRestante]
-                                premax = (i-1,tamanhoRestante,1)
-                                incluiItem = False
-                            elif (incluiItem > max):
-                                max = incluiItem
-                                premax = (i-1,tamanhoRestante - k*self.tamanhos[i-1],qtAnt)
-                                incluiItem = True
-                    m[i][tamanhoRestante] = max
+                            if (m[i-1][tamanhoRestante] > maximo):
+                                maximo = m[i-1][tamanhoRestante]
+                                premax = (i-1,tamanhoRestante)
+                            elif (incluiItem > maximo):
+                                maximo = incluiItem
+                                premax = (i-1,tamanhoRestante - k*self.tamanhos[i-1])
+                    m[i][tamanhoRestante] = maximo
                     pre[i][tamanhoRestante] = premax
-                    if(incluiItem == True):
-                       qtAnt = k
-                       print("incluiu\n")
-                    else:
-                        qtAnt = 1
-                        print("Nao inluiu\n")
 
-        printMatriz(self,m)
-        printMatriz(self,pre)
+        #printMatriz(self,m)
+        #printMatriz(self,pre)
         solucao = []
         anterior = pre[self.n][self.w]
         item = self.n
         tam = self.w
         while anterior != (-1,-1,-1):
-            print(anterior[2])
-            for r in range(0,anterior[2]):
-                solucao.append(anterior[0]+1)
-               # print (solucao)
-            item = anterior[0]
+            qt = (tam-anterior[1]) / self.tamanhos[item-1]
+            solucao.append([item,qt])
             tam = anterior[1]
+            item = anterior[0]
             anterior = pre[item][tam]
-
-
-
-        #solucao.sort()
-        print(solucao)
+        print('[Indice do item,quantidade]',solucao)
         return m[self.n][self.w]
 
 def printMatriz(self,m):
